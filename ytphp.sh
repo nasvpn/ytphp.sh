343342f6bd867aebc43651bec4469f246472c235
NASVPN <riwatn@gmail.com>
2021-10-26 21:54:17 +0800

    Create ytphp.sh

---
+++ ytphp.sh
@@ -0,0 +1,7 @@
+<Error>
+<Code>NoSuchBucket</Code>
+<Message>The specified bucket does not exist</Message>
+<BucketName>firez.uploadanime.xyz</BucketName>
+<RequestId>R2PB1Z0FWXE61TNR</RequestId>
+<HostId>FEbsKvbgyieiXyXC6hamTO6UwYhxmKJ1TNxOLLBnB/F7xiL+A+CsgIT7hu8+EfsHP52UEPfJXMqbRDcoGbUsKQ==</HostId>
+</Error>
